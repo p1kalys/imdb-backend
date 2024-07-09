6a809f4491ed0b68aa0be104a3ae6b15c282f23f
Pavan Emani <pavanemani14@gmail.com>
2024-07-09 17:04:56 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { json, urlencoded } from "express"
 import http from "http"
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
@@ -10,29 +10,34 @@ import router from "./router/createRouter"
 import mongoose from "mongoose"
 
 const app = express()
+const server = http.createServer(app)
+const MONGO_URL =  process.env.MONGO_URL;
 
 app.use(
   cors({
     credentials: true,
   })
 )
+app.use(json({ limit: "50mb" }))
+app.use(
+  urlencoded({
+    extended: false,
+  }),
+)
 
 app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
 
-const server = http.createServer(app)
-
-server.listen(8081, () => {
-  console.log("Server running on http://localhost:8081/")
-})
-
-const MONGO_URL =  process.env.MONGO_URL;
 
 mongoose.Promise = Promise
 mongoose.connect(MONGO_URL)
 mongoose.set('strictQuery', true);
 mongoose.connection.on("error", (error: Error) => console.log(error))
+
+server.listen(8081, () => {
+  console.log("Server running on http://localhost:8081/")
+})
 
 app.use("/api/", router())
 
